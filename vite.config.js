5883f9987381553622a5fb4e9ef413d0891c48e1
kraftystudioph07 <kraftystudioph07@gmail.com>
2026-08-30 00:44:51 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,21 @@
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
+import { resolve } from 'node:path';
 
 export default defineConfig({
-  base: '/KraftyAR/',
+    base: '/KraftyAR/',
 
-  plugins: [
-    tailwindcss()
-  ]
+    plugins: [
+        tailwindcss()
+    ],
+
+    build: {
+        rollupOptions: {
+            input: {
+                index: resolve(__dirname, 'index.html'),
+                home: resolve(__dirname, 'home.html'),
+                dashboard: resolve(__dirname, 'dashboard.html'),
+            }
+        }
+    }
 });
